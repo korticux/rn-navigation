e70ed127409e62a85d184639fb8314eefb1193e0
korticux <luis_cortina18@hotmail.com>
2022-04-21 20:08:45 -0700

    No sale la informacion

--- screens/MealDetailscreen.js
+++ screens/MealDetailscreen.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
 
+import { MEALS } from '../data/dummy-data';
+
  const MealDetailscreen = props => {
+    const mealId = props.navigation.getParam('mealId');
+
+    const SelectMeal = MEALS.find(meal => meal.id === mealId);
+
      return (
          <View style={styles.screen}>
-             <Text>Meal details screen !</Text>
-             <Button title="Go Back to Categories" onPress={() => {
+             <Text>{selectedMeal.title}</Text>
+             <Button 
+             title="Go Back to Categories"
+              onPress={() => {
                  props.navigation.popToTop();
              }} />
          </View>
      );
  };
+
+MealDetailscreen.navigationOptions = navigationData => {
+    const mealId = navigationData.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
+    return selectedMeal;
+}
 
  const styles = StyleSheet.create({
      screen: {
