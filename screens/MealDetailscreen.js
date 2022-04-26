d6b0860206f3e5f7250fbd477bc4d679a0bde10f
korticux <luis_cortina18@hotmail.com>
2022-04-25 21:09:08 -0700

    return sintaxis arreglado

--- screens/MealDetailscreen.js
+++ screens/MealDetailscreen.js
@@ -1,55 +1,52 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';
 import HeaderButton from '../components/HeaderButton';
 
- const MealDetailscreen = props => {
-    const mealId = props.navigation.getParam('mealId');
+const MealDetailScreen = props => {
+  const mealId = props.navigation.getParam('mealId');
 
-    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
-     return (
-         <View style={styles.screen}>
-             <Text>{selectedMeal.title}</Text>
-             <Button 
-             title="Go Back to Categories"
-              onPress={() => {
-                 props.navigation.popToTop();
-             }} />
-         </View>
-     );
- };
-
-MealDetailscreen.navigationOptions = navigationData => {
-    const mealId = navigationData.navigation.getParam('mealId');
-    const selectedMeal = MEALS.find(meal => meal.id === mealId);
-
-    return selectedMeal, 
-    headerRight(<HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item 
-        title="Favorite" 
-        iconName="ios-star"
+  return (
+    <View style={styles.screen}>
+      <Text>{selectedMeal.title}</Text>
+      <Button
+        title="Go Back to Categories"
         onPress={() => {
-            console.log('Mark as favorite!');
-        }} 
-        />
-        <Item
-        title="Favorite"
-        iconName='ios-star-outline'
-        />
-        </HeaderButtons>  
-    )  
-    
+          props.navigation.popToTop();
+        }}
+      />
+    </View>
+  );
 };
 
- const styles = StyleSheet.create({
-     screen: {
-         flex: 1,
-         justifyContent: 'center',
-         alignItems: 'center'
-     }
- })
+MealDetailScreen.navigationOptions = navigationData => {
+  const mealId = navigationData.navigation.getParam('mealId');
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  return {
+    headerTitle: ()=> selectedMeal.title,
+    headerRight: ()=>
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+        />
+        <Item
+            title="Favorite"
+            iconName="ios-star-outline"/>
+      </HeaderButtons>
+  };
+};
 
- export default MealDetailscreen;
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default MealDetailScreen;
