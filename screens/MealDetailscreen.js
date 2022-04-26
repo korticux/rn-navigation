f6d9ac90e0d6e16efea9db46d8b73406ac60849f
korticux <luis_cortina18@hotmail.com>
2022-04-25 20:08:19 -0700

    Return sintaxis

--- screens/MealDetailscreen.js
+++ screens/MealDetailscreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton';
 
  const MealDetailscreen = props => {
     const mealId = props.navigation.getParam('mealId');
 
-    const SelectMeal = MEALS.find(meal => meal.id === mealId);
+    const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
      return (
          <View style={styles.screen}>
@@ -24,8 +26,23 @@ MealDetailscreen.navigationOptions = navigationData => {
     const mealId = navigationData.navigation.getParam('mealId');
     const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
-    return selectedMeal;
-}
+    return selectedMeal, 
+    headerRight(<HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item 
+        title="Favorite" 
+        iconName="ios-star"
+        onPress={() => {
+            console.log('Mark as favorite!');
+        }} 
+        />
+        <Item
+        title="Favorite"
+        iconName='ios-star-outline'
+        />
+        </HeaderButtons>  
+    )  
+    
+};
 
  const styles = StyleSheet.create({
      screen: {
